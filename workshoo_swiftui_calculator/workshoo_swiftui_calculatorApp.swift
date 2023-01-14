9d595840694ba764624eaef3171e91370ba4d09d
Levent KANTAROĞLU <leventkantaroglu@dusyeri.com.tr>
2023-01-14 10:48:40 +0300

    Initial Commit

---
+++ workshoo_swiftui_calculator/workshoo_swiftui_calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  workshoo_swiftui_calculatorApp.swift
+//  workshoo_swiftui_calculator
+//
+//  Created by Levent Kantaroğlu on 14.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct workshoo_swiftui_calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
