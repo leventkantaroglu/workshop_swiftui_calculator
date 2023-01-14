3566094074dcd79f1614dd987327f2369b9eb859
Levent KANTAROĞLU <leventkantaroglu@dusyeri.com.tr>
2023-01-14 23:18:16 +0300

    first commit

---
+++ workshoo_swiftui_calculator/workshoo_swift_calculator.swift
@@ -0,0 +1,11 @@
+// import SwiftUI
+// import UIKit // possibly optional
+//
+// @main
+// struct ProtoTimer3App: App {
+//    var body: some Scene {
+//        WindowGroup {
+//            ContentView().environmentObject(GlobalEnvironment()) // CRITICAL!!!
+//        }
+//    }
+// }
--- workshoo_swiftui_calculator/workshoo_swiftui_calculatorApp.swift
+++ workshoo_swiftui_calculator/workshoo_swiftui_calculatorApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct workshoo_swiftui_calculatorApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView().environmentObject(GlobalEnvironment())
         }
     }
 }
